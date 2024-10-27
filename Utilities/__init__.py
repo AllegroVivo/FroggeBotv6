94f56189f91abb9c8f0292c225affc1cba39317f
Stephanie Phillips <StephBotCoder@gmail.com>
2024-10-27 06:02:08 -0700

    Remove old IconParser module

--- Utilities/__init__.py
+++ Utilities/__init__.py
@@ -1,5 +1,4 @@
 from .Colors import *
 from .ErrorMessage import ErrorMessage
-from .IconParser import IconMap
 from .Utilities import Utilities
 ################################################################################
